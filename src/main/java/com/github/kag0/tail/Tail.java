e0d9ec86b4449adc59a3434012f10a3cac636ceb
Nathan Fischer <nfischer921@gmail.com>
2020-03-13 12:47:58 -0700

    update mvn compiler plugin version

--- src/main/java/com/github/kag0/tail/Tail.java
+++ src/main/java/com/github/kag0/tail/Tail.java
@@ -5,37 +5,37 @@ import java.util.function.Supplier;
 @FunctionalInterface
 public interface Tail<A> {
 
-    Tail<A> next();
+	Tail<A> next();
 
-    static <A> Tail<A> call(Supplier<Tail<A>> recursive) {
-        return recursive::get;
-    }
+	static <A> Tail<A> call(Supplier<Tail<A>> recursive) {
+		return recursive::get;
+	}
 
-    static <A> Done<A> done(A result) {
-        return new Done<>(result);
-    }
+	static <A> Done<A> done(A result) {
+		return new Done<>(result);
+	}
 
-    static <A> A evaluate(Tail<A> call) {
-        Tail<A> next = call;
-        while(!(next instanceof Done)) {
-            next = next.next();
-        }
-        return ((Done<A>) next).result;
-    }
+	static <A> A evaluate(Tail<A> call) {
+		Tail<A> next = call;
+		while(!(next instanceof Done)) {
+			next = next.next();
+		}
+		return ((Done<A>) next).result;
+	}
 
-    default A evaluate() {
-        return evaluate(this);
-    }
+	default A evaluate() {
+		return evaluate(this);
+	}
 
-    final class Done<A> implements Tail<A> {
-        public final A result;
+	final class Done<A> implements Tail<A> {
+		public final A result;
 
-        public Done(A result) {
-            this.result = result;
-        }
+		public Done(A result) {
+			this.result = result;
+		}
 
-        public Tail<A> next() {
-            return this;
-        }
-    }
+		public Tail<A> next() {
+			return this;
+		}
+	}
 }
